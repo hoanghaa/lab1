adc9227fed4dcc3094241d325e08c89d6ec0e8ca
hoangha <hoangha01627381018@gmail.com>
2024-05-13 18:30:51 +0700

    lab2-HoangHa-commit03: add entity Student

--- src/main/java/com/demo/demo/api/HomeApi.java
+++ src/main/java/com/demo/demo/api/HomeApi.java
@@ -1,27 +1,26 @@
 package com.demo.demo.api;
 
+import com.demo.demo.entity.Student;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 public class HomeApi {
     @GetMapping("getInfomation")
-    public ResponseEntity<?> doGetInfomation(@RequestParam("ten") String name
-                                            ,@RequestParam("tuoi") String age){
-        Map<String,String> result= new HashMap<String,String>();
-        result.put("ten",name);
-        result.put("tuoi",age);
-        result.put("lop","java5");
+    public ResponseEntity<?> doGetInfomation(@RequestBody Student student){
+        Map<String,Object> result= new HashMap<String,Object>();
+        try {
+            result.put("success",true);
+            result.put("message","call api success");
+            result.put("success",student);
+        }catch (Exception e){
+            result.put("success",false);
+            result.put("message","call api fail");
+            result.put("success",null);
+        }
         return ResponseEntity.ok(result);
-    }
-
-    @GetMapping("getInfomationWithPathVariable/{ten}")
-    public ResponseEntity<?> doGetInfomationWithPathVariable(@PathVariable String ten){
-        return ResponseEntity.ok(ten);
     }
 }
---
+++ src/main/java/com/demo/demo/entity/Student.java
@@ -0,0 +1,7 @@
+package com.demo.demo.entity;
+
+public class Student {
+    private String id;
+    private String name;
+    private int age;
+}
